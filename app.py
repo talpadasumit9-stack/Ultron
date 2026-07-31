f1bdaa926d526b4807f7fbe730e3bc128f50d604
talpadasumit9-stack <talpadasumit9@gmail.com>
2026-07-31 12:13:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, jsonify
+from twilio.rest import Client
 
 app = Flask(__name__)
 
-# Ultron ki Memory (Short-term)
+# --- TWILIO SETUP ---
+# Yahan apni Twilio details dalein
+TWILIO_SID = 'YOUR_TWILIO_ACCOUNT_SID'
+TWILIO_AUTH_TOKEN = 'YOUR_TWILIO_AUTH_TOKEN'
+TWILIO_NUMBER = '+1234567890' # Twilio wala number
+
+client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
 ultron_memory = []
 
 @app.route('/')
@@ -14,30 +22,35 @@ def ask_ultron():
     global ultron_memory
     data = request.json
     user_message = data.get("message", "").lower()
-    
     reply = ""
 
-    # Memory Check
-    if "tumhe kya yaad hai" in user_message or "what do you remember" in user_message:
-        if len(ultron_memory) > 0:
-            reply = "I remember our last conversation. You said: " + ultron_memory[-1]
-        else:
-            reply = "My memory banks are currently empty, Boss."
-    
-    # Theme change command acknowledge
-    elif "change theme" in user_message:
-        reply = "Switching interface to alternative 3D holographic mode, Boss."
-    
-    # Normal commands
-    elif "hello" in user_message or "hi" in user_message:
-        reply = "Hello Boss. Core systems are online."
-    else:
-        reply = f"Command received: {user_message}. Processing data."
+    # SMS Bhejne ka Logic
+    if "send message to dhaval" in user_message:
+        try:
+            # Dhaval ka real number yahan dalein (country code ke sath, jaise +91...)
+            dhaval_number = "+919876543210" 
+            msg = client.messages.create(
+                body="Hello from Ultron. This is an automated system message.",
+                from_=TWILIO_NUMBER,
+                to=dhaval_number
+            )
+            reply = "Message has been successfully sent to Dhaval, Boss."
+        except Exception as e:
+            reply = "Sorry Boss, there was an error sending the message over the Twilio network."
 
-    # Baaton ko memory mein save karna
-    if "change theme" not in user_message:
+    # Theme Logic
+    elif "change theme" in user_message or "yellow theme" in user_message:
+        reply = "Switching interface to alternative holographic mode."
+    
+    # Normal Conversation
+    elif "hello" in user_message:
+        reply = "Hello Boss. All systems are online."
+    else:
+        reply = f"Command processed: {user_message}."
+
+    # Memory
+    if "theme" not in user_message and "message" not in user_message:
         ultron_memory.append(user_message)
-        # Sirf last 5 baatein yaad rakhne ke liye taaki memory full na ho
         if len(ultron_memory) > 5:
             ultron_memory.pop(0)
 
